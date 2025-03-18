2335a14179edb5c49770dab7c286b747287a1264
Alexey P. <106820767+lehamagnum@users.noreply.github.com>
2025-03-18 12:54:49 +1000

    project creation and initial setup

---
+++ testApp2/testApp2/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testApp2
+//
+//  Created by Алексей Патрушев on 18.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
